42a0d38cb96b9f23f1c59cde916d3f776560a954
Diego Paiva <diego@fusely.pt>
2023-02-24 12:41:34 +0000

    dto likes user

--- src/likes/dto/post-like.dto.ts
+++ src/likes/dto/post-like.dto.ts
@@ -1,4 +1,11 @@
+import { IsInt } from 'class-validator';
+import { ApiProperty } from '@nestjs/swagger';
 export class LikePostDto {
+  @ApiProperty({ example: 1 })
+  @IsInt()
   userId: number;
+
+  @ApiProperty({ example: 2 })
+  @IsInt()
   postId: number;
 }
